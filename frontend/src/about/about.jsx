6ce585d193bb1584d4208d51dc87ed4305ef8bbf
costaceta <rcostadesignweb@gmail.com>
2019-01-28 12:25:52 -0300

    Todo App

---
+++ frontend/src/about/about.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import PageHeader from '../template/pageHeader';
+
+export default props => (
+   <div>
+       <PageHeader name='Sobre' small='Nós'></PageHeader>
+
+       <h2>Nossa História</h2>
+       <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Id deleniti dolorem dolores. Repudiandae, molestiae beatae, soluta architecto,
+            ut labore inventore corporis facilis sint eius harum quas? Iste ad maiores dolores.
+       </p>
+       <h2>Missão Visão</h2>
+       <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Id deleniti dolorem dolores. Repudiandae, molestiae beatae, soluta architecto,
+            ut labore inventore corporis facilis sint eius harum quas? Iste ad maiores dolores.
+       </p>
+       <h2>Imprensa</h2>
+       <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Id deleniti dolorem dolores. Repudiandae, molestiae beatae, soluta architecto,
+            ut labore inventore corporis facilis sint eius harum quas? Iste ad maiores dolores.
+       </p>
+   </div>
+)
